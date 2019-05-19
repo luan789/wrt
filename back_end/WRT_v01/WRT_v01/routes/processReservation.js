b8b1b2049d557f65d92db46f6b8d185bf9e236cc
luan789 <tivua099pvt@gmail.com>
2019-05-19 14:21:27 +0700

    modify src back

--- back_end/WRT_v01/WRT_v01/routes/processReservation.js
+++ back_end/WRT_v01/WRT_v01/routes/processReservation.js
@@ -31,4 +31,30 @@ router.get('/station', function (req, res) {
     });
 })
 
+router.get('/st1/:origin/st2/:destina', function (req, res) {
+    res.set({ 'content-type': 'application/json; charset=utf-8' });
+    var par1 = req.params.origin;
+    var par2 = req.params.destina;
+    console.log(req.params.origin);
+    pool.connect(function (err) {
+        pool.query("select * from wrt_t_schedule where id_route = (select id_route from wrt_i_route where wrt_i_route.id_origin= '" + par1 + "' and wrt_i_route.id_destinate= '" + par2 + "')",
+            function (
+                error,
+                recordset
+            ) {
+                if (error) {
+                    console.log(error);
+                    res.send({
+                        code: 400,
+                        failed: "Error ocurred"
+                    });
+                } else {
+                    res.status(200).send(JSON.stringify({ data: recordset.recordsets[0] }));
+
+                }
+            });
+
+    });
+})
+
 module.exports = router
