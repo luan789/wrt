0ad6619dd2424d96ee97dff8ff0f3ba6a499f5c7
Nguyenthile <leevt1997@gmail.com>
2019-05-20 08:08:39 +0700

    upload front_end

--- front_end/WEB/assets/js/script.js
+++ front_end/WEB/assets/js/script.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-	
-});
+var i=0;
+var locate;
+function seatClick() {
+	i++;
+	var seat= document.getElementsByClassName('seat');
+	if(i%2!=0){
+		this.classList.add('selected');
+	}
+	else{
+		this.classList.remove('selected');
+	}
+}
